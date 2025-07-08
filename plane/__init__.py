9d7c8b2c824c7561502914a35e9cfc8fdacb7582
Surya Prashanth <prashantsurya002@gmail.com>
2025-07-08 16:31:43 +0530

    feat: add README content as setup's long description
    - add action step to create a new github release
    - bump version to 0.1.2

--- plane/__init__.py
+++ plane/__init__.py
@@ -15,7 +15,7 @@
 """  # noqa: E501
 
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 # import apis into sdk package
 from plane.api.assets_api import AssetsApi
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "plane-sdk"
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 PYTHON_REQUIRES = ">=3.7"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 3.0.0",
@@ -31,6 +31,10 @@ REQUIRES = [
     "aenum"
 ]
 
+# Read README.md for long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name=NAME,
     version=VERSION,
@@ -38,14 +42,12 @@ setup(
     author="Plane",
     author_email="support@plane.so",
     url="",
-    keywords=["OpenAPI", "OpenAPI-Generator", "The Plane REST API"],
+    keywords=["The Plane REST API"],
     install_requires=REQUIRES,
     packages=find_packages(exclude=["test", "tests"]),
     include_package_data=True,
     license="GNU AGPLv3",
     long_description_content_type='text/markdown',
-    long_description="""\
-    The Plane REST API  Visit our quick start guide and full API documentation at [developers.plane.so](https://developers.plane.so/api-reference/introduction).
-    """,  # noqa: E501
+    long_description=long_description,
     package_data={"plane": ["py.typed"]},
 )
